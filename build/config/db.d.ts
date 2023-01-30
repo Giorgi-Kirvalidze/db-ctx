276303a684b75dcbe8bf53c95fe6c0baa94ce919
Zkirvaldize <giorgi.kirvalidze@georgiatech.eu>
2023-01-30 20:13:31 +0400

    test

--- build/config/db.d.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-export declare class Database {
-    private _db;
-    initDb(url: string): Promise<void>;
-    get db(): typeof mongoose;
-}
-//# sourceMappingURL=db.d.ts.map
--- build/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Database } from './config/db';
-declare class DBContext extends Database {
-    constructor();
-}
-declare const dbContext: DBContext;
-export { dbContext };
-//# sourceMappingURL=index.d.ts.map
